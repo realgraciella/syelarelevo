63e6c0768467a0c058a8206dfb34aafc4274693f
graciellarelevo@gmail.com <ITSELEY09@GMAIL.COM>
2024-07-05 12:05:37 +0800

    Push Relevo Folder

---
+++ file1.py
@@ -0,0 +1,18 @@
+# file1.py
+def factorial(n):
+ if n == 0:
+ return 1
+ else:
+ return n * factorial(n-1)
+result = factorial(5)
+print("Factorial of 5 is: {result}")
+
+
+
+def fibonacci_sequence(n):
+ sequence = [0, 1]
+ while len(sequence) < n:
+ sequence.append(sequence[-1] + sequence[-2])
+ return sequence
+result = fibonacci_sequence(10)
+print(f"Fibonacci sequence up to 10 numbers: {result}")
---
+++ file4.py
@@ -0,0 +1,4 @@
+# file4.py
+def greet(name):
+ print(f"Hello, {name}!")
+greet("Alice")
